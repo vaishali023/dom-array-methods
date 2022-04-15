6fdf9b8c22ada38007f1c0167614b8a56524f72d
vaishali023 <vaishalichaudhary011@gmail.com>
2022-04-15 12:03:19 -0400

    done with the UI of the app, started the javascript part

---
+++ script.js
@@ -0,0 +1,6 @@
+const main = document.getElementById('main');
+const addUerBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionaire = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
